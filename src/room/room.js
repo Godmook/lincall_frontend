e9bfdb4c30fb0c5b7dba64c9465d42db2368ed5a
originalchaltteokcookie <wndrnrdk@naver.com>
2022-12-01 04:01:11 +0900

    CHANGEs

--- src/room/room.js
+++ src/room/room.js
@@ -29,7 +29,27 @@ var stun_config ={
         },
     ]
 }
-
+const makeTmp = [
+    {"emotion":"none","time":1,"type":"counselor","message":" 안녕하세요 상담사 정현진입니다"},
+    {"emotion":"angry","question":"주문 취소는 어떻게 하나요?\n","answer":"1. 가게에서 주문을 접수하기 전<br />-App에서 직접 취소할 수 있습니다.<br /><br />2. 가게에서 주문을 접수한 후<br />-주문한 가게로 연락하여 취소가 가능한지 확인할 수 있습니다.\n","time":1669833421393,"type":"client","message":" 여보세요 제가 떡볶이를 시켰는데 너무 늦게 와서요 주문 취소가 가능한가요"},
+    {"emotion":"none","time":1,"type":"counselor","message":" 아 죄송합니다. 주문접수 알림톡을 받으셨다면 결제 내역 변경이 어렵습니다"},
+    {"emotion":"none","time":1,"type":"client","message":" 아니 지금 한 시간이 넘게 안하는데 그럼 미리 알려주셨어야죠"},
+    {"emotion":"none","time":1,"type":"client","message":" 환불 해주세요"},
+    {"emotion":"none","time":1,"type":"counselor","message":" 불편을 끼쳐드려 죄송합니다.규정상 환불이 어렵습니다 "},
+    {"emotion":"angry","time":1669833220479,"type":"client","message":" 그리고 숟가락도 안보내주셨잖아요 숟가락을 안보내면 어떻게 먹어요"},
+    {"emotion":"angry","time":1669833233509,"type":"client","message":" 아 그러니 깐 규정은 모르겠고 환불해주세요"},
+    {"emotion":"angry","time":1669833258827,"type":"counselor","message":" 정말 죄송합니다 숟가락이 없는 것은 환불 사유에 해당하지 않습니다"},
+    {"emotion":"none","time":1669833276684,"type":"client","message":" 죄송한게 문제가 아니고 그래서 어쩔 건데요"},
+    {"emotion":"angry","time":1669833293235,"type":"counselor","message":" 지금 문의하신 내용이 떡볶이 환불이 맞으실까요"},
+    {"emotion":"angry","time":1669833329242,"type":"client","message":" 지금 장난해요 내가 지금 돈 천 원이 아까워서 이러고 있겠어요"},
+    {"emotion":"angry","time":1669833359290,"type":"client","message":" 돈이 문제가 아니라 기분이 나빠서 그래요"},
+    {"emotion":"angry","time":1669833375098,"type":"counselor","message":" 도움을 드리지 못해서 죄송합니다 주문 톡 확인 씨 환불이 어려운 점 양해부탁드립니다"},
+    {"emotion":"none","time":1669833447488,"type":"client","message":" 야 너 내가 뭐하는 사람인줄 알아 너네 얼굴 보고도 그렇게 말할수있을거같아"},
+    {"emotion":"angry","time":1669833464570,"type":"counselor","message":" 고객님 욕설하시면 응답이 어렵습니다"},
+    {"emotion":"angry","time":1669833481163,"type":"client","message":" 그러니까 너가 그 따위 일이나 하면서 나한테 욕 얻어먹고 있는거야. 평생 그렇게 살아라"},
+    {"emotion":"angry","time":1669833501674,"type":"counselor","message":" 고객님 지속적으로 욕설하시면 통화가 종료될수 있습니다"},
+    {"emotion":"angry","time":1669833533489,"type":"client","message":" 매니저 받고 너네 회사가 그러니까 망하는거야"}
+]
 
 const Createchat = ({type,message,time,emotion,question,answer}) => {
     const [facetype,setFaceType] = useState(faMeh);
